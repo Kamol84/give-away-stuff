e6612daaed0a20b08d83d68ccd59782965aa42ad
Kamol84 <kamil84wrz>
2019-04-11 15:01:31 +0200

    first commit

---
+++ src/main/java/pl/kamol84/giveawaystuff/GiveAwayStuffApplication.java
@@ -0,0 +1,13 @@
+package pl.kamol84.giveawaystuff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiveAwayStuffApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GiveAwayStuffApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/kamol84/giveawaystuff/GiveAwayStuffApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.kamol84.giveawaystuff;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GiveAwayStuffApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
