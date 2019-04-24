312365de763269243d6546358fa334a5b0f56a28
Kamol84 <kamil84wrz>
2019-04-24 17:59:14 +0200

    add spring secure configuration and landing page.

--- src/main/java/pl/kamol84/giveawaystuff/entity/InitialDataLoader.java
+++ src/main/java/pl/kamol84/giveawaystuff/entity/InitialDataLoader.java
@@ -1,29 +1,29 @@
-package pl.kamol84.giveawaystuff.entity;
-
-import org.springframework.context.ApplicationListener;
-import org.springframework.context.event.ContextRefreshedEvent;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Component;
-import pl.kamol84.giveawaystuff.repository.RoleRepository;
-import pl.kamol84.giveawaystuff.repository.UserRepository;
-
-@Component
-public class InitialDataLoader implements ApplicationListener<ContextRefreshedEvent> {
-
-    private UserRepository userRepository;
-    private RoleRepository roleRepository;
-    private PasswordEncoder passwordEncoder;
-
-    public InitialDataLoader(UserRepository userRepository, RoleRepository roleRepository, PasswordEncoder passwordEncoder) {
-        this.userRepository = userRepository;
-        this.roleRepository = roleRepository;
-        this.passwordEncoder = passwordEncoder;
-    }
-
-    @Override
-    public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
-
-
-
-    }
-}
+//package pl.kamol84.giveawaystuff.entity;
+//
+//import org.springframework.context.ApplicationListener;
+//import org.springframework.context.event.ContextRefreshedEvent;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.springframework.stereotype.Component;
+//import pl.kamol84.giveawaystuff.repository.RoleRepository;
+//import pl.kamol84.giveawaystuff.repository.UserRepository;
+//
+//@Component
+//public class InitialDataLoader implements ApplicationListener<ContextRefreshedEvent> {
+//
+//    private UserRepository userRepository;
+//    private RoleRepository roleRepository;
+//    private PasswordEncoder passwordEncoder;
+//
+//    public InitialDataLoader(UserRepository userRepository, RoleRepository roleRepository, PasswordEncoder passwordEncoder) {
+//        this.userRepository = userRepository;
+//        this.roleRepository = roleRepository;
+//        this.passwordEncoder = passwordEncoder;
+//    }
+//
+//    @Override
+//    public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
+//
+//
+//
+//    }
+//}
